187724219235260834f70640ee2769eb5990dd89
elsa <guscaelsa@student.gu.se>
2020-04-06 17:15:22 +0200

    labb1: given files

---
+++ labb1/src/Lab1.java
@@ -0,0 +1,48 @@
+public class Lab1 {
+    /** Sorting algorithms **/
+
+    // Insertion sort.
+
+    public static void insertionSort(int[] array) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Quicksort.
+
+    public static void quickSort(int[] array) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Quicksort part of an array
+    private static void quickSort(int[] array, int begin, int end) {
+    }
+
+    // Partition part of an array, and return the index where the pivot
+    // ended up.
+    private static int partition(int[] array, int begin, int end) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Swap two elements in an array
+    private static void swap(int[] array, int i, int j) {
+        int x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+    }
+
+    // Mergesort.
+
+    public static int[] mergeSort(int[] array) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Mergesort part of an array
+    private static int[] mergeSort(int[] array, int begin, int end) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Merge two sorted arrays into one
+    private static int[] merge(int[] left, int[] right) {
+        throw new UnsupportedOperationException();
+    }
+}
