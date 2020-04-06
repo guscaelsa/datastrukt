dfe8123ffab9d46836585def99b384735f191cbd
DESKTOP-EUPRHSK\linus <zenorex1@gmail.com>
2020-04-06 19:10:22 +0200

    implementation of insertion sort

--- labb1/src/Lab1.java
+++ labb1/src/Lab1.java
@@ -1,30 +1,50 @@
-public class Lab1 {
+package com.Lab1;
+
+public class Lab1
+{
     /** Sorting algorithms **/
 
-    // Insertion sort.
+    // in-place insertion sort.
 
-    public static void insertionSort(int[] array) {
-        throw new UnsupportedOperationException();
+    public static void insertionSort(int[] array)
+    {
+        if (array == null || array.length <= 1)
+            return;
+
+        for (int i = 1; i < array.length; i++) {
+            int k = i;
+            int val = array[i];
+            // move the value upwards
+            while (k > 0 && array[k - 1] > val) {
+                array[k] = array[k - 1];
+                k--;
+            }
+            array[k] = val;
+        }
     }
 
     // Quicksort.
 
-    public static void quickSort(int[] array) {
+    public static void quickSort(int[] array)
+    {
         throw new UnsupportedOperationException();
     }
 
     // Quicksort part of an array
-    private static void quickSort(int[] array, int begin, int end) {
+    private static void quickSort(int[] array, int begin, int end)
+    {
     }
 
     // Partition part of an array, and return the index where the pivot
     // ended up.
-    private static int partition(int[] array, int begin, int end) {
+    private static int partition(int[] array, int begin, int end)
+    {
         throw new UnsupportedOperationException();
     }
 
     // Swap two elements in an array
-    private static void swap(int[] array, int i, int j) {
+    private static void swap(int[] array, int i, int j)
+    {
         int x = array[i];
         array[i] = array[j];
         array[j] = x;
@@ -32,17 +52,20 @@ public class Lab1 {
 
     // Mergesort.
 
-    public static int[] mergeSort(int[] array) {
+    public static int[] mergeSort(int[] array)
+    {
         throw new UnsupportedOperationException();
     }
 
     // Mergesort part of an array
-    private static int[] mergeSort(int[] array, int begin, int end) {
+    private static int[] mergeSort(int[] array, int begin, int end)
+    {
         throw new UnsupportedOperationException();
     }
 
     // Merge two sorted arrays into one
-    private static int[] merge(int[] left, int[] right) {
+    private static int[] merge(int[] left, int[] right)
+    {
         throw new UnsupportedOperationException();
     }
 }
