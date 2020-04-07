40d952d00d3b751733ff6a05279d0b685ae373f4
DESKTOP-EUPRHSK\linus <zenorex1@gmail.com>
2020-04-07 17:25:39 +0200

    implementation quicksort

--- labb1/src/Lab1.java
+++ labb1/src/Lab1.java
@@ -23,21 +23,42 @@ public class Lab1
 
     // Quicksort.
 
-    public static void quickSort(int[] array)
-    {
-        throw new UnsupportedOperationException();
+    public static void quickSort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+
+        quickSort(array, 0, array.length);
     }
 
+
     // Quicksort part of an array
-    private static void quickSort(int[] array, int begin, int end)
-    {
+    private static void quickSort(int[] array, int begin, int end) {
+        int pivotIndex = partition(array, begin, end);
+        if (begin < pivotIndex - 1) {
+            quickSort(array, begin, pivotIndex - 1);
+        }
+        if (pivotIndex < end){
+            quickSort(array, pivotIndex, end);
+        }
     }
 
     // Partition part of an array, and return the index where the pivot
     // ended up.
-    private static int partition(int[] array, int begin, int end)
-    {
-        throw new UnsupportedOperationException();
+    private static int partition(int[] array, int begin, int end) {
+        int pivot = array[begin];
+        int low = begin + 1;
+        int high = end;
+
+        for (int j = begin + 1; j < high; j++) {
+            if (array[j] < pivot) {
+                swap(array, low, j);
+                low++;
+            }
+        }
+
+        swap(array, begin, low - 1);
+        return low;
     }
 
     // Swap two elements in an array
